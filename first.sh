9fddb9e5ff3468aa18666e6a382e222452dc5858
s682936 <raul.ramirez@evertecinc.com>
2019-12-05 18:32:31 -0400

    ls test

--- first.sh
+++ first.sh
@@ -11,8 +11,8 @@ SYNCHPATH="$WORKINGDIR/approved"
 LOGSPATH="$WORKINGDIR/synchS3.log"
 
 BUCKETNAME="csg-dev-zfia-analytics-apdocumentsc789d3eb-v8hk9ah5euyw"
-S3APPROVED="s3://$BUCKETNAME/invoices/approved"
-S3SYNCHRONIZED="s3://$BUCKETNAME/invoices/synchronized"
+S3APPROVED="s3://$BUCKETNAME/invoices/approved/"
+S3SYNCHRONIZED="s3://$BUCKETNAME/invoices/synchronized/"
 
 CURRDATE=`date`
 
@@ -21,4 +21,4 @@ CURRDATE=`date`
 [ ! -d $SYNCHPATH ] && mkdir -p $SYNCHPATH 
 [ ! -f $LOGSPATH ] && touch $LOGSPATH
 
-#S3SYNCHRESPONSE=`aws s3 sync $S3APPROVED $SYNCHPATH`
+S3SYNCHRESPONSE=`aws s3 ls $S3APPROVED`
