52ddb6565b2cc3c7eda4b04792ffdc9c7b70f59f
s682936 <raul.ramirez@evertecinc.com>
2019-12-09 18:20:52 -0400

    get access key 02

--- Linux/get-access-keys.sh
+++ Linux/get-access-keys.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 PYTHONVERSION=`python -V 2>&1 | grep -Po '(?<=Python )(.+)'`
-ACCESSKEYID=`cat $HOME/s3sync-script/Linux/AccessKey.js | python -c "import sys, json; print json.load(sys.stdin)['AccessKey']['AccessKeyId']"`
+ACCESSKEYID=`cat $HOME/s3sync-script/Linux/AccessKey.js | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['AccessKeyId']; print(info)"`
+SECRETACCESSKEY=`cat $HOME/s3sync-script/Linux/AccessKey.js | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['SecretAccessKey']; print(info)"`
 
 echo $PYTHONVERSION
 echo $ACCESSKEYID
+echo $SECRETACCESSKEY
