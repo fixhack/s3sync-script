87518193ca1033c3a828a73201ef57e4537f28d3
s682936 <raul.ramirez@evertecinc.com>
2019-12-10 12:25:11 -0400

    role added to script

--- Linux/get-access-keys.sh
+++ Linux/get-access-keys.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 usage() {
-  echo "Usage: $0 [ -u AWS_USER_NAME ] [ -p PROFILE ]" 1>&2
+  echo "Usage: $0 [ -u AWS_USER_NAME ] [ -r ROLE_ARN ]" 1>&2
 }
 
 exit_abnormal() {
@@ -10,17 +10,17 @@ exit_abnormal() {
 }
 
 uflag=false
-pflag=false
+rflag=false
 
-while getopts ":u:p:" options; do
+while getopts ":u:r:" options; do
   case "${options}" in
     u)
       AWS_USER_NAME=${OPTARG}
       uflag=true
       ;;
-    p)
-      AWS_PROFILE=${OPTARG}
-      pflag=true
+    r)
+      AWS_ROLE_ARN=${OPTARG}
+      rflag=true
       ;;
     :)
       echo "ERROR: -${OPTARG} requires an argument."
@@ -37,21 +37,32 @@ if [ ${uflag} != true ]; then
   exit_abnormal
 fi
 
-if [ ${pflag} != true ]; then
-  echo "ERROR: option -p is required."
+if [ ${rflag} != true ]; then
+  echo "ERROR: option -r is required."
   exit_abnormal
 fi
 
-#PYTHONVERSION=`python -V 2>&1 | grep -Po '(?<=Python )(.+)'`
-AWS_COMMAND=`aws iam create-access-key --user-name ${AWS_USER_NAME} --profile ${AWS_PROFILE}`
+AWS_ASSUME_ROLE_COMMAND=`aws sts assume-role --role-arn "${AWS_ROLE_ARN}" --role-session-name AWSCLI-Session`
 
 if [ "$?" != 0 ]; then
-  echo "Error executing aws command: ${AWS_COMMAND}"
+  echo "Error executing aws assume-role command: ${AWS_CREATE_ACCESS_KEY_COMMAND}"
   exit_abnormal
 fi
 
-ACCESSKEYID=`echo ${AWS_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['AccessKeyId']; print(info)"`
-SECRETACCESSKEY=`echo ${AWS_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['SecretAccessKey']; print(info)"`
+AWS_ACCESS_KEY_ID=`echo ${AWS_ASSUME_ROLE_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['Credentials']['AccessKeyId']; print(info)"`
+AWS_SECRET_ACCESS_KEY=`echo ${AWS_ASSUME_ROLE_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['Credentials']['SecretAccessKey']; print(info)"`
+AWS_SESSION_TOKEN=`echo ${AWS_ASSUME_ROLE_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['Credentials']['SessionToken']; print(info)"`
+
+#PYTHONVERSION=`python -V 2>&1 | grep -Po '(?<=Python )(.+)'`
+AWS_CREATE_ACCESS_KEY_COMMAND=`aws iam create-access-key --user-name ${AWS_USER_NAME}`
+
+if [ "$?" != 0 ]; then
+  echo "Error executing aws create-access-key command: ${AWS_CREATE_ACCESS_KEY_COMMAND}"
+  exit_abnormal
+fi
+
+ACCESSKEYID=`echo ${AWS_CREATE_ACCESS_KEY_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['AccessKeyId']; print(info)"`
+SECRETACCESSKEY=`echo ${AWS_CREATE_ACCESS_KEY_COMMAND} | python -c "import sys, json; info = json.load(sys.stdin)['AccessKey']['SecretAccessKey']; print(info)"`
 
 cat <<EOF > ./credentials
 [default]
