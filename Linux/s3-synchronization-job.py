4c2ca9d80c345f3085742e52d2f7166d679fc792
Raul Ramirez <raul.ramirez@evertecinc.com>
2020-05-22 18:08:08 -0400

    sh to py

---
+++ Linux/s3-synchronization-job.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import getopt
+import sys
+
+options, remaining = getopt.getopt(sys.argv[1:], 'b:o:', ['bucket=', 'output-dir='])
+
+bucket=''
+outputDir=''
+
+print options
+
+if len(options) != 0:
+	for opt, arg in options:
+        	if opt in ('-b', '--bucket'):
+                	bucket = arg
+        	elif opt in ('-o', '--output-dir'):
+                	outputDir = arg
+
+print 'BUCKET = ', bucket
+print 'OUTPUT DIR = ', outputDir
