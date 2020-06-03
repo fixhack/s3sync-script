3ebd0c9b0b6297ac0d56724fbbf4e9e3a7d4da8f
s682936 <raul.ramirez@evertecinc.com>
2020-06-03 11:53:39 -0400

    Changes to sync py script

--- Linux/s3-synchronization-job.py
+++ Linux/s3-synchronization-job.py
@@ -3,19 +3,25 @@
 import getopt
 import sys
 
-options, remaining = getopt.getopt(sys.argv[1:], 'b:o:', ['bucket=', 'output-dir='])
+def main():
+	options, remaining = getopt.getopt(sys.argv[1:], 'b:o:', ['bucket=', 'output-dir='])
 
-bucket=''
-outputDir=''
+	bucket=''
+	outputDir=''
 
-print options
+	print(options)
 
-if len(options) != 0:
-	for opt, arg in options:
-        	if opt in ('-b', '--bucket'):
-                	bucket = arg
-        	elif opt in ('-o', '--output-dir'):
-                	outputDir = arg
+	if len(options) != 0:
+		for opt, arg in options:
+				if opt in ('-b', '--bucket'):
+						bucket = arg
+				elif opt in ('-o', '--output-dir'):
+						outputDir = arg
 
-print 'BUCKET = ', bucket
-print 'OUTPUT DIR = ', outputDir
+	print ('BUCKET = ', bucket)
+	print ('OUTPUT DIR = ', outputDir)
+
+
+
+if __name__ == '__main__':
+	main()
